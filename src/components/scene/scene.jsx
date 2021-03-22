26ad98cbee53b7077b9ed70fa5cc567641ebf97e
de-evg <d.e.min@yandex.ru>
2021-03-22 17:24:21 +0300

    added cube with rotate

--- src/components/scene/scene.jsx
+++ src/components/scene/scene.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import * as THREE from "three";
 
 const Scene = () => {
-  return <p>Сцена</p>;
+  let mount = useRef(null);
+  const [three, setThreeRenderer] = useState(null);
+  const [isDragging, setDragging] = useState(false);
+  const [StartCoord, setStartCoord] = useState({ startX: null, startY: null });
+
+  const handleMouseDown = useCallback(
+    (evt) => {
+      setStartCoord({ x: evt.clientX, y: evt.clientY });
+      setDragging(true);
+    },
+    [setDragging]
+  );
+
+  const handleMouseMove = useCallback(
+    (evt) => {
+      const { geometry, renderer, scene, camera } = three;
+      const { x, y } = StartCoord;
+      const shift = {
+        x: x - evt.clientX,
+        y: y - evt.clientY,
+      };
+
+      const deltaRotationQuaternion = new THREE.Quaternion().setFromEuler(
+        new THREE.Euler(shift.y * -0.0001, shift.x *  -0.0001, 0, "XYZ")
+      );
+
+      geometry.quaternion.multiplyQuaternions(
+        deltaRotationQuaternion,
+        geometry.quaternion
+      );
+      renderer.render(scene, camera);
+    },
+    [three, StartCoord]
+  );
+
+  const handleMouseUp = useCallback(() => {
+    three.renderer.domElement.removeEventListener("mousemove", handleMouseMove);
+    setDragging(false);
+  }, [three, handleMouseMove]);
+
+  useEffect(() => {
+    if (three) {
+      three.renderer.domElement.addEventListener("mousedown", handleMouseDown);
+    }
+  }, [three, handleMouseDown]);
+
+  useEffect(() => {
+    if (three && isDragging) {
+      three.renderer.domElement.addEventListener("mousemove", handleMouseMove);
+    }
+  }, [three, isDragging, handleMouseMove]);
+
+  useEffect(() => {
+    if (three && isDragging) {
+      three.renderer.domElement.addEventListener("mouseup", handleMouseUp);
+    }
+  }, [three, isDragging, handleMouseUp]);
+
+  useEffect(() => {
+    if (!three) {
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      camera.position.set(0, 0, 5);
+
+      const renderer = new THREE.WebGLRenderer({ antialias: true });
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      mount.appendChild(renderer.domElement);
+
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(geometry, material);
+      cube.rotation.x = Math.PI / 4;
+      cube.rotation.y = Math.PI / 4;
+      scene.add(cube);
+
+      var light = new THREE.PointLight(0xffffff);
+      light.position.set(-10, 15, 50);
+      scene.add(light);
+
+      renderer.render(scene, camera);
+      setThreeRenderer({ renderer, geometry: cube, scene, camera });
+    }
+  }, [three, setThreeRenderer]);
+
+  return <div ref={(ref) => (mount = ref)}></div>;
 };
 
 export default Scene;
